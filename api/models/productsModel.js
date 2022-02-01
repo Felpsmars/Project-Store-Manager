99be96d85676e722fceb2a46b282622fa5b85fdd
Felipe Martins <mfelipeluiz99@gmail.com>
2022-02-01 18:00:29 -0300

    impl: productsModel.js

---
+++ api/models/productsModel.js
@@ -0,0 +1,64 @@
+const connection = require('./connection');
+
+const getAll = async () => {
+  const [rows] = await connection.execute('SELECT * FROM products');
+  return rows;
+};
+
+const getByName = async (name) => {
+  const [rows] = await connection.execute(
+    'SELECT * FROM products WHERE name = ?',
+    [name],
+  );
+  return rows[0];
+};
+
+const getById = async (id) => {
+  const [rows] = await connection.execute(
+    'SELECT * FROM products WHERE id = ?',
+    [id],
+  );
+  if (!rows.length) return null;
+  return rows[0];
+};
+
+const create = async ({ name, quantity }) => {
+  const [rows] = await connection.execute(
+    'INSERT INTO products (name, quantity) VALUES (?, ?)',
+    [name, quantity],
+  );
+  return {
+    id: rows.insertId,
+    name,
+    quantity,
+  };
+};
+
+const update = async ({ id, name, quantity }) => {
+  await connection.execute(
+    'UPDATE products SET name = ?, quantity = ? WHERE id = ?',
+    [name, quantity, id],
+  );
+  return {
+    id,
+    name,
+    quantity,
+  };
+};
+
+const remove = async (id) => {
+  const product = await getById(id);
+  if (!product) return null;
+
+  await connection.execute('DELETE FROM products WHERE id = ?', [id]);
+  return product;
+};
+
+module.exports = {
+  getAll,
+  getById,
+  getByName,
+  create,
+  update,
+  remove,
+};
